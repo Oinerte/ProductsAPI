afe31e3fa2191db7600a1d98555d5967f783413d
Lauro Meireles <oinerte@pop-os.localdomain>
2022-06-23 11:03:57 -0300

    desafio 2

--- projeto02.js
+++ projeto02.js
@@ -1,28 +1,30 @@
-const array = ['ale', 'ana', 2, [true, true]]
+const array = ['a', 'b', 2, 50, [true, true, false], {'chave': 'valor'}, [1, [2, "c"]]]
+// const array = 'oi'
 const strings = []
 const numbers = []
 const Booleans = []
+const objects = []
 
-for(i=0;i<array.length;i++){
-    if(typeof array[i] == 'string'){
-        strings.push(array[i])
-    }else if(typeof array[i] == 'number'){
-        numbers.push(array[i])
-    }else if (typeof array[i] == 'object'){
-        desmembraArray(array[i])
-    }else if (typeof array[i] == 'boolean'){
-        Booleans.push(array[i])
-    }else {
-        console.log('karai');
+function separaArrays(array) {
+    for(i=0;i<array.length;i++){
+        if(typeof array[i] == 'string'){
+            strings.push(array[i])
+        }else if(typeof array[i] == 'number'){
+            numbers.push(array[i])
+        }else if (Array.isArray(array[i])){
+                for(j=0;j<array[i].length;j++){
+                     array.push(array[i][j])}
+        }else if (typeof array[i] == 'boolean'){
+            Booleans.push(array[i])
+        }else if(typeof array[i] == "object"){
+            objects.push(array[i])
+        }
     }
 }
+separaArrays(array)
 
-function desmembraArray(object) {
-    for(i=0;i<object.length;i++){
-        array.push(object[i])
-    }
-}
 
 console.log(strings);
 console.log(numbers);
 console.log(Booleans);
+console.log(objects);
