26cefab2a116c5f72fdc502c9da96a59c4aa3845
Lauro Meireles <oinerte@pop-os.localdomain>
2022-06-28 08:14:22 -0300

    baixando index.test.js

---
+++ index.test.js
@@ -0,0 +1,26 @@
+const app = require('./index')
+
+test('expect the index to correctly crate a person', () => {
+ const person = [{id: 1, name: 'Lauro', age:  15, gender: 'M', isActive: true}]
+ expect(app.createPerson('Lauro', 15, 'M')).toEqual(person[0])
+ app.persons.pop()
+})
+
+test('expect the function to deactivate a person', () => {
+    app.persons.push({id: 1, name: 'Lauro', age:  15, gender: 'M', isActive: true})
+    expect(app.deactivatePersonById(1)).toBe(false)
+    app.persons.pop()
+})
+
+test('expect the function to only return active persons ordered by their age', () => {
+    app.persons.push(
+    { id: 1, name: 'Lauro', age: 15, gender: 'M', isActive: true}, 
+    { id: 3, name: 'Bruno', age: 24, gender: 'M', isActive: false}, 
+    { id: 2, name: 'Iza', age: 22, gender: 'F', isActive: true})
+    for(i = 0; i < app.persons.length; i++){
+        expect(app.getActivePersons()[i].isActive).not.toBe(false)
+    }
+    expect(app.getActivePersons()[1].age < app.getActivePersons()[0] && 
+        app.getActivePersons()[1].age > app.getActivePersons()[2].age).toBe(true)
+    app.persons.pop()
+})
